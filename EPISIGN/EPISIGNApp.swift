8e24b2b631799e0db8c24ae3425e940a08f5113a
nlnguyen <linhbaljeet0208@gmail.com>
2026-04-24 09:24:58 +0200

    Initial Commit

---
+++ EPISIGN/EPISIGNApp.swift
@@ -0,0 +1,17 @@
+//
+//  EPISIGNApp.swift
+//  EPISIGN
+//
+//  Created by Nhat Linh on 24/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct EPISIGNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
